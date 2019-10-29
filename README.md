72425976872952597ee5ce395fe59e75de11d0b1
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2019-10-29 14:11:35 +0100

    Latest schema

--- README.md
+++ README.md
@@ -25,4 +25,18 @@ By default, PostgreSQL will be available with this config:
 
 To update schema, please run `schemaSpy`. The result will be in `/tmp/qbase-schema`
 
-![](./schema.png) 
+![](schema.png) 
+
+## DB Dump
+
+When the database is filled with data, run:
+
+``` sh
+docker exec --user postgres qbase pg_dump --data-only --role qbase qbase | xz --threads=0 > $(date -I)-qbase.sql.xz
+```
+
+To restore the dump, start with Postgres DB __without__ `qbase` user and __without__ `qbase` database. Then, run:
+``` sh
+cat docker-entrypoint-initdb.d/1-db-create.sql | psql
+xzcat $(date -I)-qbase.sql.xz | psql
+```
--- docker-entrypoint-initdb.d/1-db-create.sql
+++ docker-entrypoint-initdb.d/1-db-create.sql
@@ -1,92 +1,99 @@
-CREATE DATABASE qbase;
+CREATE ROLE qbase WITH CREATEDB LOGIN PASSWORD 'Jellux37';
+
+CREATE DATABASE qbase WITH OWNER qbase;
 
 \connect qbase;
 
-CREATE TYPE onz AS ENUM (
-  '+O', '-O', '+N', '-N', '+Z', '-Z'
+SET ROLE qbase;
+
+CREATE TYPE onz AS ENUM ('O+', 'O-', 'N+', 'N-', 'Z+', 'Z-');
+
+CREATE TYPE onzm AS ENUM ('Op', 'Oa', 'Oh', 'Np', 'Na', 'Nh', 'Zp', 'Za', 'Zh', 'Mp', 'Ma', 'Mh', 'n/a');
+
+CREATE TYPE molecule AS ENUM ('RNA', 'DNA', 'Other');
+
+CREATE TYPE experiment AS ENUM ('X-Ray', 'NMR', 'Other');
+
+CREATE TYPE stericity AS ENUM ('cis', 'trans');
+
+CREATE TYPE edge AS ENUM ('Watson-Crick', 'Hoogsteen', 'Sugar');
+
+CREATE TYPE glycosidic_bond AS ENUM ('anti', 'syn', '...');
+
+CREATE TYPE direction AS ENUM ('parallel', 'antiparallel', 'hybrid', 'n/a');
+
+CREATE TABLE pdb
+(
+    id            SERIAL PRIMARY KEY,
+    identifier    CHAR(4)    NOT NULL,
+    assembly      INTEGER    NOT NULL,
+    experiment    EXPERIMENT NOT NULL,
+    resolution    REAL,
+    visualization TEXT       NOT NULL,
+    arc_diagram   TEXT       NOT NULL
 );
 
-CREATE TYPE molecule AS ENUM (
-  'RNA', 'DNA', 'Other'
+CREATE TABLE nucleotide
+(
+    id              SERIAL PRIMARY KEY,
+    pdb_id          INTEGER         NOT NULL REFERENCES pdb (id),
+    model           INTEGER         NOT NULL,
+    chain           TEXT            NOT NULL,
+    number          INTEGER         NOT NULL,
+    icode           CHAR,
+    molecule        MOLECULE        NOT NULL,
+    full_name       TEXT            NOT NULL,
+    short_name      CHAR            NOT NULL,
+    glycosidic_bond GLYCOSIDIC_BOND NOT NULL,
+    coordinates     TEXT            NOT NULL
 );
 
-CREATE TYPE experiment AS ENUM (
-  'X-Ray', 'NMR', 'Other'
+CREATE TABLE helix
+(
+    id            SERIAL PRIMARY KEY,
+    visualization TEXT NOT NULL,
+    arc_diagram   TEXT NOT NULL
 );
 
-CREATE TYPE stericity AS ENUM (
-  'cis', 'trans'
+CREATE TABLE quadruplex
+(
+    id            SERIAL PRIMARY KEY,
+    helix_id      INTEGER NOT NULL REFERENCES helix (id),
+    onzm          ONZM    NOT NULL,
+    visualization TEXT    NOT NULL,
+    arc_diagram   TEXT    NOT NULL
 );
 
-CREATE TYPE edge AS ENUM (
-  'Watson-Crick', 'Hoogsteen', 'Sugar'
+CREATE TABLE tetrad
+(
+    id            SERIAL PRIMARY KEY,
+    quadruplex_id INTEGER NOT NULL REFERENCES quadruplex (id),
+    nt1_id        INTEGER NOT NULL REFERENCES nucleotide (id),
+    nt2_id        INTEGER NOT NULL REFERENCES nucleotide (id),
+    nt3_id        INTEGER NOT NULL REFERENCES nucleotide (id),
+    nt4_id        INTEGER NOT NULL REFERENCES nucleotide (id),
+    onz           ONZ     NOT NULL,
+    planarity     REAL    NOT NULL,
+    visualization TEXT    NOT NULL,
+    arc_diagram   TEXT    NOT NULL
 );
 
-CREATE TYPE strand_direction AS ENUM (
-  'parallel', 'antiparallel', 'mixed'
+CREATE TABLE base_pair
+(
+    id        SERIAL PRIMARY KEY,
+    nt1_id    INTEGER   NOT NULL REFERENCES nucleotide (id),
+    nt2_id    INTEGER   NOT NULL REFERENCES nucleotide (id),
+    stericity STERICITY NOT NULL,
+    edge5     EDGE      NOT NULL,
+    edge3     EDGE      NOT NULL
 );
 
-CREATE TYPE glycosidic_bond AS ENUM (
-  'anti', 'syn'
+CREATE TABLE tetrad_pair
+(
+    id         SERIAL PRIMARY KEY,
+    tetrad1_id INTEGER   NOT NULL REFERENCES tetrad (id),
+    tetrad2_id INTEGER   NOT NULL REFERENCES tetrad (id),
+    direction  DIRECTION NOT NULL,
+    rise       REAL      NOT NULL,
+    twist      REAL      NOT NULL
 );
-
-CREATE TABLE pdb (
-  id            CHAR(4) PRIMARY KEY,
-  assembly      INTEGER,
-  experiment    EXPERIMENT NOT NULL,
-  resolution    REAL,
-  visualization TEXT       NOT NULL
-);
-
-CREATE TABLE nucleotide (
-  id              SERIAL PRIMARY KEY,
-  pdb_id          CHAR(4)         NOT NULL REFERENCES pdb (id),
-  model           INTEGER,
-  chain           TEXT            NOT NULL,
-  number          INTEGER         NOT NULL,
-  icode           CHAR,
-  molecule        MOLECULE        NOT NULL,
-  full_name       TEXT            NOT NULL,
-  short_name      CHAR            NOT NULL,
-  glycosidic_bond GLYCOSIDIC_BOND NOT NULL,
-  coordinates     TEXT
-);
-
-CREATE TABLE tetrade (
-  id        SERIAL PRIMARY KEY,
-  nt1_id    INTEGER NOT NULL REFERENCES nucleotide (id),
-  nt2_id    INTEGER NOT NULL REFERENCES nucleotide (id),
-  nt3_id    INTEGER NOT NULL REFERENCES nucleotide (id),
-  nt4_id    INTEGER NOT NULL REFERENCES nucleotide (id),
-  planarity REAL    NOT NULL,
-  onz       ONZ
-);
-
-CREATE TABLE quadruplex (
-  id               SERIAL PRIMARY KEY,
-  strand_direction STRAND_DIRECTION NOT NULL,
-  visualization    TEXT             NOT NULL
-);
-
-CREATE TABLE quadruplex_tetrade (
-  id            SERIAL PRIMARY KEY,
-  quadruplex_id INTEGER NOT NULL REFERENCES quadruplex (id),
-  tetrade_id    INTEGER NOT NULL REFERENCES tetrade (id)
-);
-
-CREATE TABLE base_pair (
-  id        SERIAL PRIMARY KEY,
-  nt1_id    INTEGER   NOT NULL REFERENCES nucleotide (id),
-  nt2_id    INTEGER   NOT NULL REFERENCES nucleotide (id),
-  stericity STERICITY NOT NULL,
-  edge5     EDGE      NOT NULL,
-  edge3     EDGE      NOT NULL
-);
-
-CREATE TABLE tetrade_stack (
-  id          SERIAL PRIMARY KEY,
-  tetrade1_id INTEGER NOT NULL REFERENCES tetrade (id),
-  tetrade2_id INTEGER NOT NULL REFERENCES tetrade (id),
-  rise        REAL    NOT NULL,
-  twist       REAL    NOT NULL
-)
